a30b103dc954d13c46b590b5b8f5cdb45a6072aa
sudatra <mail2sudatra@gmail.com>
2025-03-09 20:00:11 +0530

    added webhook for syncing users with clerk

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/server/db";
+
+export const POST = async (req: Request) => {
+  const { data } = await req.json();
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+
+  await db.user.create({
+    data: {
+      id,
+      emailAddress,
+      firstName,
+      lastName,
+      imageUrl
+    }
+  });
+
+  return new Response('Webhook request received', { status: 200 });
+}
